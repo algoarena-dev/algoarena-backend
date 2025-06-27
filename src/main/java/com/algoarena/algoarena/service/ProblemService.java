e45f8a99944e902ffbbe4eca2a870acc945a528a
Ulvi Sadigzade <ulvisadiqzade05@gmail.com>
2025-06-27 23:14:44 +0400

    Add ProblemService

---
+++ src/main/java/com/algoarena/algoarena/service/ProblemService.java
@@ -0,0 +1,11 @@
+package com.algoarena.algoarena.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class ProblemService {
+}
