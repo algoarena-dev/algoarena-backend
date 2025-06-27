5e96ce92e69c3c389377ffc6a7aa3ca0fad6edbd
Ulvi Sadigzade <ulvisadiqzade05@gmail.com>
2025-06-27 14:42:43 +0400

    Initial commit

---
+++ src/main/java/com/algoarena/algoarena/AlgoarenaApplication.java
@@ -0,0 +1,13 @@
+package com.algoarena.algoarena;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoarenaApplication {
+	public static void main(String[] args) {
+
+		SpringApplication.run(AlgoarenaApplication.class, args);
+	}
+}
