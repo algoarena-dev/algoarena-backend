5a274c02ea0b584f404ffec315b3f73dada8b617
Ulvi Sadigzade <ulvisadiqzade05@gmail.com>
2025-06-28 23:28:40 +0400

    Implement ProblemController methods

--- src/main/java/com/algoarena/algoarena/controller/ProblemController.java
+++ src/main/java/com/algoarena/algoarena/controller/ProblemController.java
@@ -3,6 +3,8 @@ package com.algoarena.algoarena.controller;
 
 import com.algoarena.algoarena.dto.ProblemRequestDto;
 import com.algoarena.algoarena.dto.ProblemResponseDto;
+import com.algoarena.algoarena.service.ProblemService;
+import com.algoarena.algoarena.service.impl.JpaProblemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,33 +15,35 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/problems")
 public class ProblemController {
+    private final ProblemService problemService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProblemResponseDto> getAllProblems(){
-
+        return problemService.getAllProblems();
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ProblemResponseDto getProblemById(@PathVariable Long id){
+        return problemService.getProblemById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ProblemResponseDto createProblem(@RequestBody ProblemRequestDto problemRequestDto){
-
+        return problemService.createProblem(problemRequestDto);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ProblemResponseDto updateProblemById(@PathVariable Long id,@RequestBody ProblemRequestDto problemRequestDto){
-
+        return problemService.updateProblemById(id,problemRequestDto);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteProblemById(@PathVariable Long id){
-
+        problemService.deleteProblemById(id);
     }
 }
